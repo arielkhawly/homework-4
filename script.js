7c0ffe93ab02d8dc0e35a3de20a6d9714270f414
Ariel Khawly <akhawly59@gmail.com>
2020-01-25 19:14:05 -0800

    file creation, question creation

---
+++ script.js
@@ -0,0 +1,9 @@
+//Allow User to start the quiz
+//Change screens to show question one
+// Create buttons for each possible answer
+//Tell com which is the right and which are the wrong answers
+//Have an alert to say if the person got the question right or wrong
+//Factor it into their score
+
+
+
